ff69dfd7ddeb3d38bc754e6462919cfee38a8274
z58occc <a0981583030@gmail.com>
2025-09-17 22:31:01 +0800

    Updates

--- assets/onboarding/character-creating-CA3Qxswx.js
+++
@@ -1,1 +0,0 @@
-import"../main-Ct0kAut6.js";setTimeout(()=>{window.location.replace("../onboarding/character.html")},5e3);
---
+++ assets/onboarding/character-creating-ZbW_Mzs8.js
@@ -0,0 +1,1 @@
+import"../main-CZwpoKa6.js";setTimeout(()=>{window.location.replace("../onboarding/character.html")},5e3);
