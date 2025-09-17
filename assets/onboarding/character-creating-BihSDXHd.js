de9b60facb2c2966959772089a5c7027fbacb22a
z58occc <a0981583030@gmail.com>
2025-09-17 22:43:00 +0800

    Updates

---
+++ assets/onboarding/character-creating-BihSDXHd.js
@@ -0,0 +1,1 @@
+import"../main-itV7PL6W.js";setTimeout(()=>{window.location.replace("../onboarding/character.html")},5e3);
--- assets/onboarding/character-creating-ZbW_Mzs8.js
+++
@@ -1,1 +0,0 @@
-import"../main-CZwpoKa6.js";setTimeout(()=>{window.location.replace("../onboarding/character.html")},5e3);
