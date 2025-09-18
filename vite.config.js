8e753a53646d40fc2a6ee47133daea161658c649
tanukili <108317269+tanukili@users.noreply.github.com>
2025-09-18 08:27:25 +0800

    chore: 安裝 React

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-import { glob } from 'glob';
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { glob } from "glob";
+import react from "@vitejs/plugin-react";
 
-import liveReload from 'vite-plugin-live-reload';
+import liveReload from "vite-plugin-live-reload";
 
 function moveOutputPlugin() {
   return {
-    name: 'move-output',
-    enforce: 'post',
-    apply: 'build',
+    name: "move-output",
+    enforce: "post",
+    apply: "build",
     async generateBundle(options, bundle) {
       for (const fileName in bundle) {
-        if (fileName.startsWith('pages/')) {
-          const newFileName = fileName.slice('pages/'.length);
+        if (fileName.startsWith("pages/")) {
+          const newFileName = fileName.slice("pages/".length);
           bundle[fileName].fileName = newFileName;
         }
       }
@@ -25,30 +26,31 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/MEELO/',
+  base: "/MEELO/",
   plugins: [
-    liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
+    liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
     moveOutputPlugin(),
+    react(),
   ],
   server: {
     // 啟動 server 時預設開啟的頁面
-    open: 'pages/index.html',
+    open: "pages/index.html",
   },
   build: {
     rollupOptions: {
       input: Object.fromEntries(
         glob
-          .sync('pages/**/*.html')
+          .sync("pages/**/*.html")
           .map((file) => [
             path.relative(
-              'pages',
+              "pages",
               file.slice(0, file.length - path.extname(file).length)
             ),
             fileURLToPath(new URL(file, import.meta.url)),
           ])
       ),
     },
-    outDir: 'dist',
+    outDir: "dist",
   },
 });
